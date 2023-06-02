6be7a43d07272d74ee593ab122e353355f0df507
Ekaterina Belova <kbelovaa17@gmail.com>
2023-06-02 18:01:08 +0300

    Start project

---
+++ Task4.java
@@ -0,0 +1,7 @@
+//\u000Apublic class Task4{
+    //\u000Apublic static void main(String[] args){
+        //\u000A\u002F\u002A
+        console.log("Itransition");//\u002A\u002F
+        //\u000ASystem.out.println("Itransition");
+    //\u000A}
+//\u000A}
